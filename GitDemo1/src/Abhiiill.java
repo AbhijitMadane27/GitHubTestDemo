ece0d2782098d215e7f16af41e9b4d8a5b344158
AbhijitMadane27 <madane.abhijit27@gmail.com>
2022-06-15 22:48:37 +0530

    by bab

---
+++ GitDemo1/src/Abhiiill.java
@@ -0,0 +1,31 @@
+
+public class Abhiiill 
+{
+
+	 public static void main(String[] args) 
+	   {
+		
+		   System.out.println("hiiiiii");
+	}
+	   
+	   
+	   
+	   public void name()
+	   {
+		
+		   System.out.println("my name us abhhh");
+	}
+	   
+	   public void m3()
+	   {
+		
+		   System.out.println("here is m3");
+	}
+		
+		 public void m4()
+	   {
+		
+		   System.out.println("here is m3");
+	}
+
+}
